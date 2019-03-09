324084c3ca1d1b4e63d57910bebf46240f64e4dc
Thanasis <x@X>
2019-03-09 10:15:34 +0200

    malakas beryllium defconfig stock + script updated

--- k.sh
+++ k.sh
@@ -3,7 +3,7 @@
 rm -rf /out
 make kernelversion
 make clean && make mrproper
-export ARCH=arm64 && export SUBARCH=arm64 beryllium_defconfig
+export ARCH=arm64 && export SUBARCH=arm64 malakas_beryllium_defconfig
 export CROSS_COMPILE=~/TOOLCHAIN/gcc/bin/aarch64-linux-gnu-
 export CC=~/TOOLCHAIN/clang9/bin/clang 
 export CLANG_TRIPLE=~/TOOLCHAIN/sdclang/aarch64-linux-android
@@ -14,7 +14,7 @@ export <OUT_DIR> =out
 #make menuconfig
 mkdir -p out
 make O=out clean
-make O=out beryllium_defconfig
+make O=out malakas_beryllium_defconfig
 make -j$(nproc --all) O=out 
 mv out/arch/arm64/boot/Image.gz-dtb ~/TOOLCHAIN/anykernel/Image.gz-dtb
 rm -rf out
