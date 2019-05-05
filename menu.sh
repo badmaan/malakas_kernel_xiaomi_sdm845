12695f52d9e66d0bba31510b5513a4b4965e5568
thanas <x@X>
2019-05-05 18:26:02 +0300

    update menuconfig script

--- menu.sh
+++ menu.sh
@@ -4,5 +4,5 @@ make kernelversion
 export ARCH=arm64 && export SUBARCH=arm64 malakas_beryllium_defconfig
 make malakas_beryllium_defconfig
 make menuconfig
-mv .config /arch/arm64/configs/malakas_defconfig
+mv .config arch/arm64/configs/malakas_beryllium_defconfig2
 
