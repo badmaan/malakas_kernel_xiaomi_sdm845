c50f63c91d625ecec8f925a79e488f65e52f6621
thanas <x@X>
2019-05-09 21:51:06 +0300

    init: Revert to s2idle suspend
    Google was right; disabling secondary CPUs via hotplug during suspend
    increases idle drain. This is probably because our firmware will detect
    CPUs that have been idling for a long time and put them into a low-power
    retention state that does not use much more power than hotplugging them
    altogether. See the following issue for more information: https://github.com/sonyxperiadev/kernel/pull/1914
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

---
+++ flasher/95-proton.sh
@@ -0,0 +1,48 @@
+#!/system/bin/sh
+
+#
+# Wait for /data to be mounted
+#
+
+while ! mountpoint -q /data; do
+	sleep 1
+done
+
+#
+# Cleanup
+#
+
+# Remove old backup DTBOs
+rm -f /data/adb/dtbo_a.orig.img /data/adb/dtbo_b.orig.img
+
+# Check if Proton is no longer installed
+if ! grep -q Proton /proc/version; then
+	# Remove the custom PowerHAL config
+	rm -f /data/adb/magisk_simple/vendor/etc/powerhint.json
+
+	# Remove this init script
+	rm -f /data/adb/service.d/95-proton.sh
+
+	# Abort and do not apply tweaks
+	exit 0
+fi
+
+#
+# Wait for Android to finish booting
+#
+
+while [ "$(getprop sys.boot_completed)" != 1 ]; do
+	sleep 2
+done
+
+# Wait for init to finish processing all boot_completed actions
+sleep 2
+
+#
+# Apply overrides and tweaks
+#
+
+echo 85 > /proc/sys/vm/swappiness # Reduce kswapd cpu usage
+echo $(cat /sys/module/cpu_input_boost/parameters/input_boost_duration) > /sys/class/drm/card0/device/idle_timeout_ms # Reduce default PowerHAL interaction boost timeout
+echo 1 > /sys/module/printk/parameters/console_suspend # Marginally reduce suspend latency
+echo 3000 > /proc/sys/vm/dirty_expire_centisecs # Allow dirty writeback to be less frequent
