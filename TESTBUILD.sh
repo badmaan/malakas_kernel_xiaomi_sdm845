431bf1d6d788d101d7acd20dab3f3f76dc57b6db
thanas <x@X>
2019-05-17 11:53:00 +0300

    testbuild script malakas

---
+++ TESTBUILD.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+rm -rf /out
+make kernelversion
+export ARCH=arm64 && export SUBARCH=arm64 malakas_beryllium_defconfig
+#/scripts/config -e CLANG_LTO
+#make ARCH=arm64 oldconfig
+export CROSS_COMPILE=~/TOOLCHAIN/gcc/bin/aarch64-linux-gnu- 
+#export CC=~/TOOLCHAIN/dtc/bin/clang 
+#export CLANG_TRIPLE=~/TOOLCHAIN/gcc/bin/aarch64-linux-gnu-
+#export LD=~/TOOLCHAIN/gcc/bin/aarch64-linux-gnu-ld.gold
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
+mkdir -p out
+make O=out clean
+make O=out malakas_beryllium_defconfig
+make -j$(nproc --all) O=out 
+mv out/arch/arm64/boot/Image.gz-dtb ~/TOOLCHAIN/AK2TEST/Image.gz-dtb
+rm -rf out
+cd ~/TOOLCHAIN/AK2TEST
+zip -r MLX_TEST65hz-$(date +"%Y-%m-%d").zip *
+rm -rf Image.gz-dtb
+mv MLX_TEST65hz-$(date +"%Y-%m-%d").zip ~/Desktop
+cd ~/Desktop
+adb kill-server
+adb tcpip 5555
+adb connect 192.168.3.101:5555
+sleep 2
+adb push MLX_TEST65hz-$(date +"%Y-%m-%d").zip /sdcard/MLX_TEST65hz-$(date +"%Y-%m-%d").zip
+adb reboot recovery
+
+
