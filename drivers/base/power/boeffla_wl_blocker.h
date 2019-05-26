fd5f57baae4da5b8557b768b96abd2230b2fef40
thanas <x@X>
2019-05-26 09:40:04 +0300

    Revert "malakas block wakelocks"
    This reverts commit ede5648126c3a4e4cb9b30dee23973e09dbb0662.

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			"898000.qcom,qup_uart;IPA_WS;NETLINK;[timerfd];c440000.qcom,spmi:qcom,pmi8998@2:qcom,qpnp-smb2;enable_ipa_ws;enable_netlink_ws;enable_netmgr_wl_ws;enable_qcom_rx_wakelock_ws;enable_timerfd_ws;enable_wlan_etscan_wl_ws;enable_wlan_wow_wl_ws;enable_wlan_ws;hal_bluetooth_lock;netmgr_wl;qcom_rx_wakelock;sensor_ind;wcnss_filtr_lock;wlan;wlan_extscan_wl;wlan_ipa;wlan_pno_wl;wlan_wow_wl"
+#define LIST_WL_DEFAULT			""
 
 #define LENGTH_LIST_WL			255
 #define LENGTH_LIST_WL_DEFAULT		100
