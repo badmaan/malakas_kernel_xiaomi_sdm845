322f0ab87eb8c82783a86fc209627453a4964836
Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
2019-05-26 18:17:14 +0100

    kernel: Boost only CPUs when a zygote-forked process becomes a top app
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -64,7 +64,6 @@
 #include <linux/file.h>
 #include <linux/binfmts.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 #include <net/sock.h>
 
 #define CREATE_TRACE_POINTS
@@ -2966,7 +2965,6 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
 		is_zygote_pid(tsk->parent->pid)) {
 		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
 	}
 
 	put_task_struct(tsk);
