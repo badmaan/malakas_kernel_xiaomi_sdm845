ac58c042d243bd949443eea97c4122c67807c756
Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
2019-06-02 15:16:11 +0100

    Kernel: Reduce app launch boost duration
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1968,8 +1968,8 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		cpu_input_boost_kick_max(32);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 32);
 	}
 
 	/*
