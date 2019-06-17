1a5b5dd584eebda2c81eb7b4fc5bbe3b11894c88
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-17 00:16:24 -0400

    Revert "binder: Reserve caches for small, high-frequency memory allocations"
    This reverts commit 1a0f30c1112a533643aea220a5c7f270ff59f8aa.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5919,13 +5919,9 @@ static int __init binder_init(void)
 	struct binder_device *device;
 	struct hlist_node *tmp;
 
-	ret = binder_create_pools();
-	if (ret)
-		return ret;
-
 	ret = binder_alloc_shrinker_init();
 	if (ret)
-		goto err_workqueue_init_failed;
+		return ret;
 
 	atomic_set(&binder_transaction_log.cur, ~0U);
 	atomic_set(&binder_transaction_log_failed.cur, ~0U);
@@ -5994,8 +5990,6 @@ err_init_binder_device_failed:
 
 err_alloc_device_names_failed:
 	debugfs_remove_recursive(binder_debugfs_dir_entry_root);
-err_workqueue_init_failed:
-	binder_destroy_pools();
 
 	return ret;
 }
