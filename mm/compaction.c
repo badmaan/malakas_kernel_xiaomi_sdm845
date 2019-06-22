6c3e1af9b1aa0fbcbbc64f2365af777eb6eb20db
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-23 04:53:27 +0900

    mm: compaction: compact zswap pool as well
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1856,6 +1856,7 @@ static void compact_node(int nid)
 	}
 }
 
+extern void zswap_compact(void);
 /* Compact all nodes in the system */
 static void compact_nodes(void)
 {
@@ -1866,6 +1867,8 @@ static void compact_nodes(void)
 
 	for_each_online_node(nid)
 		compact_node(nid);
+
+	zswap_compact();
 }
 
 static void do_compaction(struct work_struct *work)
