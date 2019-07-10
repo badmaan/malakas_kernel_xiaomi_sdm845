3af51c3c0afa1d581dce8667220805b03b958f1e
thanasis <thanasis>
2019-07-11 00:00:18 +0300

    Revert "Merge branch 'android-4.9' of https://android.googlesource.com/kernel/common into malakas"
    This reverts commit a8b05ae9241438b643f73e25463d68b9d396c1f2.

--- drivers/firmware/efi/libstub/efi-stub-helper.c
+++ drivers/firmware/efi/libstub/efi-stub-helper.c
@@ -41,6 +41,13 @@ static unsigned long __chunk_size = EFI_READ_CHUNK_SIZE;
 #define EFI_ALLOC_ALIGN		EFI_PAGE_SIZE
 #endif
 
+static int __section(.data) __nokaslr;
+
+int __pure nokaslr(void)
+{
+	return __nokaslr;
+}
+
 #define EFI_MMAP_NR_SLACK_SLOTS	8
 
 struct file_info {
