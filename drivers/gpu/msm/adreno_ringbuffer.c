4c87eae68bcaca527fd8896d0e18d6a1b48ffab0
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-30 14:19:59 -0700

    msm: kgsl: Avoid dynamically allocating small command buffers
    Most command buffers here are rather small (fewer than 256 words); it's
    a waste of time to dynamically allocate memory for such a small buffer
    when it could easily fit on the stack.
    
    Conditionally using an on-stack command buffer when the size is small
    enough eliminates the need for using a dynamically-allocated buffer most
    of the time, reducing GPU command submission latency.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: thanasxda <15927885+thanasxda@users.noreply.github.com>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -884,7 +884,7 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 	struct kgsl_memobj_node *ib;
 	unsigned int numibs = 0;
 	unsigned int *link;
-	unsigned int link_onstack[SZ_256] __aligned(8);
+	unsigned int link_onstack[SZ_256] __aligned(sizeof(long));
 	unsigned int *cmds;
 	struct kgsl_context *context;
 	struct adreno_context *drawctxt;
@@ -1005,15 +1005,14 @@ int adreno_ringbuffer_submitcmd(struct adreno_device *adreno_dev,
 		if (gpudev->preemption_yield_enable)
 			dwords += 8;
 
-	/*
-	 * Prior to SQE FW version 1.49, there was only one marker for
-	 * both preemption and IFPC. Only include the IB1LIST markers if
-	 * we are using a firmware that supports them.
-	 */
-	if (gpudev->set_marker && numibs && adreno_is_a6xx(adreno_dev) &&
-			((fw->version & 0xFFF) >= 0x149)) {
-		set_ib1list_marker = true;
-		dwords += 4;
+	if (dwords <= ARRAY_SIZE(link_onstack)) {
+		link = link_onstack;
+	} else {
+		link = kmalloc(sizeof(unsigned int) * dwords, GFP_KERNEL);
+		if (!link) {
+			ret = -ENOMEM;
+			goto done;
+		}
 	}
 
 	if (gpudev->ccu_invalidate)
@@ -1155,7 +1154,7 @@ done:
 	trace_kgsl_issueibcmds(device, context->id, numibs, drawobj->timestamp,
 			drawobj->flags, ret, drawctxt->type);
 
-	if (unlikely(link != link_onstack))
+	if (link != link_onstack)
 		kfree(link);
 	return ret;
 }
