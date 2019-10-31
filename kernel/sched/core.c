23931bc08117586a3d0d539da54020a1cad23f2f
thanasxda <15927885+thanasxda@users.noreply.github.com>
2019-10-31 12:21:19 +0200

    Merge 'android-4.9-q'

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3299,6 +3299,7 @@ void scheduler_tick(void)
 	curr->sched_class->task_tick(rq, curr, 0);
 	cpu_load_update_active(rq);
 	calc_global_load_tick(rq);
+	psi_task_tick(rq);
 
 	early_notif = early_detection_notify(rq, wallclock);
 	if (early_notif)
@@ -3627,7 +3628,6 @@ static void __sched notrace __schedule(bool preempt)
 	 */
 	raw_spin_lock(&rq->lock);
 	rq_pin_lock(rq, &rf);
-	smp_mb__after_spinlock();
 
 	rq->clock_skip_update <<= 1; /* promote REQ to ACT */
 
